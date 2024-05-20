b6bb3f65596bb061e48c96199f4f50a2a4f02629
DenisGradov <varonapika@gmail.com>
2024-05-20 23:49:15 +0200

    Верстка правой части (без адаптива)

--- src/App.jsx
+++ src/App.jsx
@@ -108,7 +108,221 @@ function App() {
             </div>
           </div>
         </div>
-        <div className={style["wrapper-right"]}></div>
+        <div className={style["wrapper-right"]}>
+          <h2 className={style["wrapper-right__welcome"]}>Hello Evano 👋🏼,</h2>
+          <div className={style["allCustomers"]}>
+            <div className={style["allCustomers-top"]}>
+              <div className={style["allCustomers-top-textBlock"]}>
+                <h3 className={style["textBlock__title"]}>All Customers</h3>
+                <h3 className={style["textBlock__greenText"]}>
+                  Active Members
+                </h3>
+              </div>
+              <div className={style["searchBlock"]}>
+                <input
+                  className={style["searchBlock__input"]}
+                  placeholder="Search"
+                ></input>
+                <img
+                  className={style["searchBlock__icon"]}
+                  src="/assets/icons/search 1.png"
+                />
+              </div>
+            </div>
+            <div className={style["allCustomers-columnTitles"]}>
+              <h4 className={style["allCustomers-columnTitles__text"]}>
+                Customer Name
+              </h4>
+              <h4 className={style["allCustomers-columnTitles__text"]}>
+                Company
+              </h4>
+              <h4 className={style["allCustomers-columnTitles__text"]}>
+                Phone Number
+              </h4>
+              <h4 className={style["allCustomers-columnTitles__text"]}>
+                Email
+              </h4>
+              <h4 className={style["allCustomers-columnTitles__text"]}>
+                Country
+              </h4>
+              <h4 className={style["allCustomers-columnTitles__text"]}>
+                Status
+              </h4>
+            </div>
+            <div className={style["allCustomers__bigLine"]}></div>
+            <div className={style["allCustomers-list"]}>
+              <div className={style["allCustomers-customer"]}>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  Jane Cooper
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  Microsoft
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  (225) 555-0118
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  jane@microsoft.com
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  United States
+                </h3>
+                <h3 className={style["allCustomers-customer__active"]}>
+                  Active
+                </h3>
+              </div>
+              <div className={style["allCustomers__smallLine"]}></div>
+              <div className={style["allCustomers-customer"]}>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  Floyd Miles
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>Yahoo</h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  (205) 555-0100
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  floyd@yahoo.com
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  Kiribati
+                </h3>
+                <h3 className={style["allCustomers-customer__notActive"]}>
+                  Inactive
+                </h3>
+              </div>
+              <div className={style["allCustomers__smallLine"]}></div>
+              <div className={style["allCustomers-customer"]}>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  Ronald Richards
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>Adobe</h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  (302) 555-0107
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  ronald@adobe.com
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>Israel</h3>
+                <h3 className={style["allCustomers-customer__notActive"]}>
+                  Inactive
+                </h3>
+              </div>
+              <div className={style["allCustomers__smallLine"]}></div>
+              <div className={style["allCustomers-customer"]}>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  Marvin McKinney
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>Tesla</h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  (252) 555-0126
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  marvin@tesla.com
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>Iran</h3>
+                <h3 className={style["allCustomers-customer__active"]}>
+                  Active
+                </h3>
+              </div>
+              <div className={style["allCustomers__smallLine"]}></div>
+              <div className={style["allCustomers-customer"]}>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  Jerome Bell
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>Google</h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  (629) 555-0129
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  jerome@google.com
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  Réunion
+                </h3>
+                <h3 className={style["allCustomers-customer__active"]}>
+                  Active
+                </h3>
+              </div>
+              <div className={style["allCustomers__smallLine"]}></div>
+              <div className={style["allCustomers-customer"]}>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  Kathryn Murphy
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  Microsoft
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  (406) 555-0120
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  kathryn@microsoft.com
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  Curaçao
+                </h3>
+                <h3 className={style["allCustomers-customer__active"]}>
+                  Active
+                </h3>
+              </div>
+              <div className={style["allCustomers__smallLine"]}></div>
+              <div className={style["allCustomers-customer"]}>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  Jacob Jones
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>Yahoo</h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  (208) 555-0112
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  jacob@yahoo.com
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>Brazil</h3>
+                <h3 className={style["allCustomers-customer__active"]}>
+                  Active
+                </h3>
+              </div>
+              <div className={style["allCustomers__smallLine"]}></div>
+              <div className={style["allCustomers-customer"]}>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  Kristin Watson
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  Facebook
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  (704) 555-0127
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  kristin@facebook.com
+                </h3>
+                <h3 className={style["allCustomers-customer__info"]}>
+                  Åland Islands
+                </h3>
+                <h3 className={style["allCustomers-customer__notActive"]}>
+                  Inactive
+                </h3>
+              </div>
+              <div className={style["allCustomers__smallLine"]}></div>
+            </div>
+            <div className={style["allCustomers-pagination"]}>
+              <h3 className={style["pagination__dataInfo"]}>
+                Showing data 1 to 8 of 256K entries
+              </h3>
+              <div className={style["pagination-buttons"]}>
+                <div className={style["pagination-buttons__button"]}>{"<"}</div>
+                <div className={style["pagination-buttons__activeButton"]}>
+                  1
+                </div>
+                <div className={style["pagination-buttons__button"]}>2</div>
+                <div className={style["pagination-buttons__button"]}>3</div>
+                <div className={style["pagination-buttons__button"]}>4</div>
+                <h4 className={style["pagination-buttons__text"]}>...</h4>
+                <div className={style["pagination-buttons__button"]}>40</div>
+                <div className={style["pagination-buttons__button"]}>{">"}</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
